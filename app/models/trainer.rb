6ec103f70d7bd3151b87ec54f755c2b40326301e
ClaFiore <claborghini@gmail.com>
2020-08-20 14:09:01 -0400

    seed ok

--- app/models/trainer.rb
+++ app/models/trainer.rb
@@ -1,5 +1,5 @@
 class Trainer < ApplicationRecord
     has_many :pokemons
     validates :name, uniqueness: {case_sensitive: false}
-    validates :pokemons, length: { minimum: 1, maximum: 6 , message: "Allowed Per Team Is Maximum 6"}
+    validates :pokemons, length: { minimum: 0, maximum: 6 , message: "Allowed Per Team Is Maximum 6"}
 end
